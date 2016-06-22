ddb7899530901d952b0ecb445127aa7980124872
TejaswiR <tejaswi.ramisetty@gmail.com>
2016-06-22 21:58:27 +0530

    added form and also controller

--- scripts/app.js
+++ scripts/app.js
@@ -10,3 +10,14 @@ $routeProvider
   })
 
 });
+
+angularApp.controller("MainController",['$resource',
+function($resource){
+   var vm=this;
+ vm.getData=function(){
+  var first = $resource('http://api.census.gov/data/timeseries/idb/1year?get=AREA_KM2,NAME,AGE,POP');
+   vm.firstResponse = first.query({FIPS:vm.CountryName,time:vm.Year,key:'479f7cd05dea5959866c96bf97c6a72c45272e5d'});
+   console.log(vm.firstResponse);
+ }
+
+}]);
